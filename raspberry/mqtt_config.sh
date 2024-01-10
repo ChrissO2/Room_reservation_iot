9ea62e892ff6014a093c723dc67b9159a1a5a841
Michał Klemens <95657053+michalK00@users.noreply.github.com>
2024-01-10 09:21:27 +0100

    Update mqtt_config.sh

--- raspberry/mqtt_config.sh
+++ raspberry/mqtt_config.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo systemctl start mosquitto.service
-
 echo "allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
 echo "listener 1883 0.0.0.0" >> /etc/mosquitto/mosquitto.conf
 
-sudo systemctl restart mosquitto.service
+sudo systemctl start mosquitto.service
 
 echo "Script finished"
