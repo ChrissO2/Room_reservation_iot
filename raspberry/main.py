8fbc1c9663401ed2cefb5152273e61a9ee184fb2
Adam <al.lo3opole@gmail.com>
2024-01-11 10:16:41 +0100

    Update current state of room on oled

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,7 +1,11 @@
 import rfid_card_event
 import mqtt_connection
+import oled
+import current_room_state
 
 def main():
     mqtt_connection.initialize_mqtt_connection()
+    current_room_state.connect_to_room_state_broker()
     while True:
-        rfid_card_event.interpret_rfid_read();
+        rfid_card_event.interpret_rfid_read()
+        oled.update_oled()
