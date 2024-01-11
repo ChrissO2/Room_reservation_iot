bd2337be5ad2e71bd735c6a63ca5f20e36d66b72
Adam <al.lo3opole@gmail.com>
2024-01-11 10:00:15 +0100

    update display interface

--- raspberry/oled.py
+++ raspberry/oled.py
@@ -5,55 +5,20 @@ import board
 import adafruit_bme280.advanced as adafruit_bme280
 import busio
 
-def sensors():
-    i2c = busio.I2C(board.SCL, board.SDA)
-    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
+# def sensors():
+#     i2c = busio.I2C(board.SCL, board.SDA)
+#     bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
 
-    bme280.sea_level_pressure = 1013.25
-    bme280.standby_period = adafruit_bme280.STANDBY_TC_500
-    bme280.iir_filter = adafruit_bme280.IIR_FILTER_X16
-    bme280.overscan_pressure = adafruit_bme280.OVERSCAN_X16
-    bme280.overscan_humidity = adafruit_bme280.OVERSCAN_X1
-    bme280.overscan_temperature = adafruit_bme280.OVERSCAN_X2
-    return {'TemperatureBME280' : round(bme280.temperature, 2), 
-            'humidity':  round(bme280.humidity,2 ), 
-            'pressure':  round(bme280.pressure, 2)
-    }
-
-# def display(disp, parameters, old_parameters):
-#     image1 = Image.new("RGB", (disp.width, disp.height), "WHITE")
-#     draw = ImageDraw.Draw(image1)
-#     fontLarge = ImageFont.truetype('./lib/oled/Font.ttf', 20)
-#     fontSmall = ImageFont.truetype('./lib/oled/Font.ttf', 13)
-
-#     image_temperature = Image.open('./temperature.png')
-#     image_temperature = image_temperature.resize((15, 10))
-
-#     image_humidity = Image.open('./humidity.png')
-#     image_humidity = image_humidity.resize((15, 10))
-
-#     image_pressure = Image.open('./pressure.png')
-#     image_pressure = image_pressure.resize((15, 10))
-
-#     if old_parameters["TemperatureBME280"] != parameters["TemperatureBME280"]:
-#         draw.text((17, 0), f'T: {parameters["TemperatureBME280"]}', font=fontSmall, fill="RED")
-#     else:
-#         draw.text((17, 0), f'T: {old_parameters["TemperatureBME280"]}', font=fontSmall, fill="RED")
-#     image1.paste(image_temperature, (0, 0))
-    
-#     if old_parameters["humidity"] != parameters["humidity"]:
-#        draw.text((17, 25), f'H: {parameters["humidity"]}', font=fontSmall, fill="BLUE")
-#     else:
-#         draw.text((17, 25), f'H: {old_parameters["humidity"]}', font=fontSmall, fill="BLUE")
-#     image1.paste(image_humidity, (0, 25))
-
-#     if old_parameters["pressure"] != parameters["pressure"]:
-#        draw.text((17, 50), f'P: {parameters["pressure"]}', font=fontSmall, fill="GREEN")
-#     else:
-#         draw.text((17, 50), f'P: {old_parameters["pressure"]}', font=fontSmall, fill="GREEN")
-#     image1.paste(image_pressure, (0, 50))
-
-#     disp.ShowImage(image1, 0, 0)
+#     bme280.sea_level_pressure = 1013.25
+#     bme280.standby_period = adafruit_bme280.STANDBY_TC_500
+#     bme280.iir_filter = adafruit_bme280.IIR_FILTER_X16
+#     bme280.overscan_pressure = adafruit_bme280.OVERSCAN_X16
+#     bme280.overscan_humidity = adafruit_bme280.OVERSCAN_X1
+#     bme280.overscan_temperature = adafruit_bme280.OVERSCAN_X2
+#     return {'TemperatureBME280' : round(bme280.temperature, 2), 
+#             'humidity':  round(bme280.humidity,2 ), 
+#             'pressure':  round(bme280.pressure, 2)
+#     }
 
 def is_room_free(parameters):
     return parameters["is_free"]
@@ -62,6 +27,15 @@ def has_state_changed(parameters, old_parameters):
     return old_parameters["is_free"] != parameters["is_free"] or old_parameters["mode"] != parameters["mode"] or old_parameters["msg"] != parameters["msg"]
 
 def display(disp, parameters, old_parameters):
+  
+    if has_state_changed(parameters, old_parameters):
+        if parameters["mode"] == 'default':
+            handle_default_mode(parameters, disp)
+        elif parameters["mode"] == 'input':
+            handle_input_mode(parameters, disp)
+
+
+def handle_default_mode(parameters, disp):
     image1 = Image.new("RGB", (disp.width, disp.height), "WHITE")
     draw = ImageDraw.Draw(image1)
     fontLarge = ImageFont.truetype('./lib/oled/Font.ttf', 20)
@@ -69,68 +43,23 @@ def display(disp, parameters, old_parameters):
 
     image = Image.open('./assets/green_checkmark.png') if is_room_free(parameters) else Image.open('./assets/red_cross.png')
     image = image.resize((15, 10))
-    if has_state_changed():
-        image1.paste(image, (0, 0))
-
-    if has_state_changed(parameters, old_parameters):
-        draw.text((17, 0), parameters["msg"], font=fontSmall, fill="RED")
+    image1.paste(image, (0, 0))
+    draw.text((17, 0), parameters["msg"], font=fontSmall, fill="RED")
 
     disp.ShowImage(image1, 0, 0)
 
-    # if is_room_free(old_parameters) != is_room_free(parameters) and is_room_free(parameters):
-    #     draw.text((17, 0), parameters.msg, font=fontSmall, fill="GREEN")
-    # elif is_room_free(old_parameters) != is_room_free(parameters) and not is_room_free(parameters):
-    #     draw.text((17, 0), parameters.msg, font=fontSmall, fill="RED")
-        
-    # image_temperature = Image.open('./temperature.png')
-    # image_temperature = image_temperature.resize((15, 10))
 
-    # image_humidity = Image.open('./humidity.png')
-    # image_humidity = image_humidity.resize((15, 10))
+def handle_input_mode(paramaters, disp):
+    image1 = Image.new("RGB", (disp.width, disp.height), "WHITE")
+    draw = ImageDraw.Draw(image1)
+    fontLarge = ImageFont.truetype('./lib/oled/Font.ttf', 20)
+    fontSmall = ImageFont.truetype('./lib/oled/Font.ttf', 13)
 
-    # image_pressure = Image.open('./pressure.png')
-    # image_pressure = image_pressure.resize((15, 10))
+    draw.text((0, 0), "Ustaw spotkanie: ", font=fontSmall, fill="BLACK")
+    draw.text((17, 0), parameters["msg"], font=fontSmall, fill="RED")
 
-    # if old_parameters["TemperatureBME280"] != parameters["TemperatureBME280"]:
-    #     draw.text((17, 0), f'T: {parameters["TemperatureBME280"]}', font=fontSmall, fill="RED")
-    # else:
-    #     draw.text((17, 0), f'T: {old_parameters["TemperatureBME280"]}', font=fontSmall, fill="RED")
-    # image1.paste(image_temperature, (0, 0))
-    
-    # if old_parameters["humidity"] != parameters["humidity"]:
-    #    draw.text((17, 25), f'H: {parameters["humidity"]}', font=fontSmall, fill="BLUE")
-    # else:
-    #     draw.text((17, 25), f'H: {old_parameters["humidity"]}', font=fontSmall, fill="BLUE")
-    # image1.paste(image_humidity, (0, 25))
+    disp.ShowImage(image1, 0, 0)
 
-    # if old_parameters["pressure"] != parameters["pressure"]:
-    #    draw.text((17, 50), f'P: {parameters["pressure"]}', font=fontSmall, fill="GREEN")
-    # else:
-    #     draw.text((17, 50), f'P: {old_parameters["pressure"]}', font=fontSmall, fill="GREEN")
-    # image1.paste(image_pressure, (0, 50))
-
-    
-
-# if __name__ == "__main__":
-#     print("\nProgram started")
-#     disp = SSD1331.SSD1331()
-
-#     disp.Init()
-
-#     disp.clear()
-
-#     old_parameters = {'TemperatureBME280': 0.0,
-#                       'humidity': 0.0,
-#                       'pressure': 0.0}
-#     parameters = {'TemperatureBME280': 0.0,
-#                       'humidity': 0.0,
-#                       'pressure': 0.0}
-#     while(True):
-#         old_parameters = parameters
-#         parameters = sensors()
-#         display(disp, parameters, old_parameters)
-
-# mode is either 'default' or 'input' 
 
 old_parameters = {'is_free': True, 'msg': 0, 'mode': 'default'}
 parameters = {'is_free': True, 'msg': 0, 'mode': 'default'}
