a9e6eae08694059f8a5743bb46b59e3f9e5160ec
Krzysztof Otreba <krzysztof.otreba02@gmail.com>
2024-01-11 09:30:23 +0100

    sketch for input mode

--- raspberry/input_mode.py
+++ raspberry/input_mode.py
@@ -3,30 +3,45 @@ from datetime import datetime, timedelta
 
 import RPi.GPIO as GPIO
 import board
-import neopixel
 
 from config import *
 
+execute = True
 
-MIN_POSITION = 0
-MAX_POSITION = 7
+
+chosen_time = datetime.now()
+INTERVAL = timedelta(minutes=5)
 
 position = 3
 
 def handle_encoder_left(channel):
-    global position
+    global chosen_time
     right = GPIO.input(encoderRight)
-    if not right and position < MAX_POSITION:
-        position += 1
-    elif right and position > MIN_POSITION:
-        position -= 1
+    if not right and (chosen_time + INTERVAL).date() == datetime.now().today():
+        chosen_time += INTERVAL
+    elif right and (chosen_time - INTERVAL).date() == datetime.now().today():
+        chosen_time -= INTERVAL
+    #call oled
+        
+
+def redButtonPressed(channel):
+    global execute
+    execute = False
+    
      
+def greenButtonPressed(channel):
+    #call backend and wait
+    pass
+
 
 def set_hour():
+    global execute
     start_time = datetime.now()
     chosen_time = start_time
-    
-    GPIO.add_event_detect(encoderLeft, GPIO.FALLING, callback=handle_encoder_left, bouncetime=200)
 
-    while datetime.now() - start_time > timedelta(0, 180):
+    GPIO.add_event_detect(encoderLeft, GPIO.FALLING, callback=handle_encoder_left, bouncetime=200)
+    GPIO.add_event_detect(buttonRed, GPIO.FALLING, callback=redButtonPressed, bouncetime=200)
+    GPIO.add_event_detect(buttonGreen, GPIO.FALLING, callback=greenButtonPressed, bouncetime=200)
+
+    while execute and datetime.now() - start_time < timedelta(minutes=3):
         pass
