1fc4a02c9018010de3fafff57a9b47efcffadae1
Krzysztof Otreba <krzysztof.otreba02@gmail.com>
2024-01-11 11:02:56 +0100

    added imports to rfid_cart_event

--- raspberry/rfid_card_event.py
+++ raspberry/rfid_card_event.py
@@ -1,9 +1,24 @@
+from datetime import datetime, timedelta
+import logging
+
 import mqtt_connection
+import RPi.GPIO as GPIO
+from mfrc522 import MFRC522
+
+from config import *
 
 
 global_mifare_reader = MFRC522()
 global_activate_time = None
 readRfidCards = {}
+
+
+def blinkLed(state):
+    GPIO.output(led3, state)
+
+
+def buzz(state):
+    GPIO.output(buzzerPin, not state)
 
 
 def interpret_rfid_read(callbacks):
