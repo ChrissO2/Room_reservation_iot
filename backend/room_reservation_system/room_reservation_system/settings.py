f25b79bb61bf3654e4bc8340ea707668da704096
Krzysztof Otreba <krzysztof.otreba02@gmail.com>
2024-01-15 14:57:26 +0100

    added frontend to allowed origins in settings

--- backend/room_reservation_system/room_reservation_system/settings.py
+++ backend/room_reservation_system/room_reservation_system/settings.py
@@ -27,6 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Application definition
 
