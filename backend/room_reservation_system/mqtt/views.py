97122a0985a5867ca55dd8963ab74466d2d9af2a
karol <karolina.majkowska@o2.pl>
2024-01-15 18:34:16 +0100

    to do

--- backend/room_reservation_system/mqtt/views.py
+++ backend/room_reservation_system/mqtt/views.py
@@ -1,3 +1,12 @@
+import json
+import sqlite3
+
+from mqtt_connection import client
 from django.shortcuts import render
 
 # Create your views here.
+# TODO save to the database
+def save_message_to_database(request):
+    request_data = json.loads(request.body)
+    rc, mid = client.publish(request_data['topic'], request_data['payload'])
+
