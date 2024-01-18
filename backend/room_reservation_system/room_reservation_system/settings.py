ea8c1a859d36e94a0e9582ff85a34cdff57ba9c5
Adam <al.lo3opole@gmail.com>
2024-01-18 21:04:52 +0100

    Add frontend origin to cors

--- backend/room_reservation_system/room_reservation_system/settings.py
+++ backend/room_reservation_system/room_reservation_system/settings.py
@@ -27,11 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = ["*"]
-
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+CORS_ALLOWED_ORIGINS = [
+    "http://62.171.156.180:3000",
+    "http://127.0.0.1:3000",
 ]
+
+# CORS_ORIGIN_WHITELIST = [
+#     "http://localhost:3000",
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
