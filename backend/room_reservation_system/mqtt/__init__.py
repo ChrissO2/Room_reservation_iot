327e1a02cb51d37e253b2a329251aba5a17bedbf
karol <karolina.majkowska@o2.pl>
2024-01-19 18:54:36 +0100

    fix

--- backend/room_reservation_system/mqtt/__init__.py
+++ backend/room_reservation_system/mqtt/__init__.py
@@ -1,2 +1,6 @@
 import paho.mqtt.client as mqtt
+from room_reservation_system.settings import *
+
+from mqtt.mqtt_connection import send_message_of_availability
+mqtt.Client().connect(MQTT_SERVER)
 mqtt.client.loop_start()
