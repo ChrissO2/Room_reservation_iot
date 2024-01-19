c75fb3e9e9b5140902c702589da97bf7fa6a0030
karol <karolina.majkowska@o2.pl>
2024-01-19 18:17:31 +0100

    channel publishers

--- backend/room_reservation_system/mqtt/mqtt_connection.py
+++ backend/room_reservation_system/mqtt/mqtt_connection.py
@@ -1,8 +1,10 @@
 import datetime
+import json
 
 import paho.mqtt.client as mqtt
 from room_reservation_system import settings
 from base.models import Room, Meeting
+from threading import Thread
 
 TIMESTAMP_SEND_ROOM_AVAILABLE = 10
 LAST_SEND_MESSAGE = 0
@@ -11,12 +13,16 @@ LAST_SEND_MESSAGE = 0
 def on_connect(mqtt_client, userdata, flags, rc):
     if rc == 0:
         print('Connected successfully')
-        mqtt_client.subscribe('django/mqtt')
-    else:
+        mqtt.client.subscribe('new_meeting')
+        thread = Thread(target=send_message_of_availability)
+        thread.daemon = True
+        thread.start()
         print('Bad connection. Code:', rc)
 
 
 def on_message(msg):
+    # if msg.topic == 'new_meeting':
+    #     meeting_add_rfid()
     print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
 
 
@@ -31,19 +37,25 @@ client.connect(
 )
 
 
-def send_message_of_availability(rfid_reader_id):
-    current_time = datetime.datetime.now
-    global LAST_SEND_MESSAGE
-    if current_time - LAST_SEND_MESSAGE >= TIMESTAMP_SEND_ROOM_AVAILABLE:
-        LAST_SEND_MESSAGE = current_time
-        room_id = Room.objects.get(rfid_reader_id=rfid_reader_id).id
-        room_available = check_room_is_available(room_id)
-        client.publish("room_state", room_available)
+# TODO działa tylko dla jednego pokoju w bazie
+def send_message_of_availability():
+    while True:
+        current_time = datetime.datetime.now
+        global LAST_SEND_MESSAGE
+        if current_time - LAST_SEND_MESSAGE >= TIMESTAMP_SEND_ROOM_AVAILABLE:
+            LAST_SEND_MESSAGE = current_time
+            room_id = Room.objects.first().id
+            room_available = check_room_is_available(room_id)
+            payload = {
+                "is_free": room_available,
+                "msg": "wolne" if room_available else "zajete"
+            }
+            client.publish("room_state", json.dumps(payload))
 
 
 def check_room_is_available(room_id):
     if not Room.objects.filter(id=room_id).exists():
-        raise ValueError("Room does not exist.")
+        return False
 
     current_time = datetime.datetime.now()
     room_available = Meeting.objects.filter(
