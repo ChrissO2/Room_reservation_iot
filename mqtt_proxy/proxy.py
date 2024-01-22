58c9bc43d4795c82a369429732f6bba3671dc8e4
karol <karolina.majkowska@o2.pl>
2024-01-22 17:29:12 +0100

    mqtt

--- mqtt_proxy/proxy.py
+++ mqtt_proxy/proxy.py
@@ -8,10 +8,14 @@ import json
 BACKEND_IP = "62.171.156.180"
 BROKER_IP = "62.171.156.180"
 
+LAST_SEND = 0
+SEND_TIMESTAMP = 10
+
 connection = http.client.HTTPConnection(BACKEND_IP, 8000, timeout=10)
 
 DEFAULT_HEADERS = {"Content-type": "application/json"}
 DEFAULT_RFID_ID = 1
+
 
 def is_room_free_at(start_date, end_date):
     json_data = json.dumps(
@@ -28,10 +32,7 @@ def is_room_free_at(start_date, end_date):
     return response_data["is_free"]
 
 
-
-
-
-def call_worker():
+def room_state():
     room_available = is_room_free_at(datetime.now().isoformat(), datetime.now().isoformat())
     payload = {
         "is_free": room_available,
@@ -41,16 +42,37 @@ def call_worker():
     client.publish("room_state", json.dumps(payload))
 
 
+def add_participant_to_meeting():
+    parameters = json.message.payload.decode("utf-8")
+    rfid = parameters["rfid_reader_id"]
+    card_id = parameters["card_id"]
+    time_registration = parameters["time"]
+    json_data = json.dumps(
+        {
+            "rfid_card_id": rfid,
+            "card_id": card_id,
+            "time": time_registration,
+        }
+    )
+    connection.request("POST", "/api/new_meeting_participant_rfid", json_data, DEFAULT_HEADERS)
+    response = connection.getresponse()
+    print(response)
+    
+
 def connect_to_broker():
     client.connect(BROKER_IP)
-    call_worker()
+    client.subscribe("participant_registration")
+    client.on_message = add_participant_to_meeting
+    client.loop_start()
 
 
 if __name__ == "__main__":
     client = mqtt.Client()
     try:
+        connect_to_broker()
         while True:
-            connect_to_broker()
-            sleep(5)
+            if datetime.now() - LAST_SEND >= SEND_TIMESTAMP:
+                LAST_SEND = datetime.now
+                room_state()
     except Exception as e:
         print(e)
