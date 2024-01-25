7d45556254bbd2b2f0628a6d5d4474f0f61af10b
Adam <al.lo3opole@gmail.com>
2024-01-25 02:02:29 +0100

    Add MQTT over TCP

--- mqtt_proxy/proxy.py
+++ mqtt_proxy/proxy.py
@@ -1,18 +1,20 @@
 import http.client
 from datetime import datetime, timedelta
 from time import sleep
-
+import ssl
 import paho.mqtt.client as mqtt
 import json
 
 BACKEND_IP = "62.171.156.180"
-BROKER_IP = "62.171.156.180"
+BROKER_IP = "meeting-system.rolo-labs.xyz"
 
 LAST_SEND = 0
 SEND_TIMESTAMP = 10
 
 DEFAULT_HEADERS = {"Content-type": "application/json"}
 DEFAULT_RFID_ID = 1
+
+server_cert_path = "/home/app/webapp/ISRG_Root_X1.pem"
 
 
 def is_room_free_at(start_date, end_date):
@@ -29,16 +31,18 @@ def is_room_free_at(start_date, end_date):
     print(response)
     response_data = json.loads(response.read().decode())
     print(response_data)
-    return response_data.get('is_free')
+    return response_data.get("is_free")
 
 
 def room_state():
     current_time = datetime.now()
-    room_available = is_room_free_at(datetime.now().isoformat(), (datetime.now() + timedelta(seconds=5)).isoformat())
+    room_available = is_room_free_at(
+        datetime.now().isoformat(), (datetime.now() + timedelta(seconds=5)).isoformat()
+    )
     payload = {
         "is_free": room_available,
         "msg": "wolne" if room_available else "zajete",
-        "mode": "default"
+        "mode": "default",
     }
     client.publish("room_state", json.dumps(payload))
 
@@ -49,31 +53,40 @@ def add_participant_to_meeting(client, userdata, message):
     card_id = parameters["card_id"]
     time_registration = parameters["time"]
     json_data = json.dumps(
-        {
-            "rfid_card_id": rfid,
-            "card_id": card_id,
-            "time": time_registration
-        }
+        {"rfid_card_id": rfid, "card_id": card_id, "time": time_registration}
     )
     connection_participant = http.client.HTTPConnection(BACKEND_IP, 8000, timeout=10)
-    connection_participant.request("POST", "/api/new_meeting_participant_rfid", json_data, DEFAULT_HEADERS)
+    connection_participant.request(
+        "POST", "/api/new_meeting_participant_rfid", json_data, DEFAULT_HEADERS
+    )
     response = connection_participant.getresponse()
     print(response)
     response_data = json.loads(response.read().decode())
     print(response_data)
-    
+
 
 def connect_to_broker():
-    client_subscribe.connect(BROKER_IP)
-    client.connect(BROKER_IP)
+    client_subscribe.connect(BROKER_IP, 8883)
+    client.connect(BROKER_IP, 8883)
     client_subscribe.subscribe("participant_registration")
     client_subscribe.on_message = add_participant_to_meeting
     client_subscribe.loop_start()
 
 
+def set_tls(client):
+    client.tls_set(
+        ca_certs=server_cert_path,
+        tls_version=ssl.PROTOCOL_TLSv1_2,
+    )
+    return client
+
+
 if __name__ == "__main__":
     client = mqtt.Client()
     client_subscribe = mqtt.Client()
+    set_tls(client)
+    set_tls(client_subscribe)
+
     try:
         connect_to_broker()
         while True:
