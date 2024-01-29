25ba03b72ed87fae247da494e73c3493104ba164
MichalK <mklemens48@gmail.com>
2024-01-29 14:57:48 +0100

    extended token lifetime to 15 minutes

--- backend/room_reservation_system/room_reservation_system/settings.py
+++ backend/room_reservation_system/room_reservation_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +152,7 @@ MQTT_PORT = 1883
 MQTT_KEEPALIVE = 60
 MQTT_USER = ""
 MQTT_PASSWORD = ""
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15)
+}
